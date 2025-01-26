fff5b73f1aeb475b1d39b847d171a6f8a716dc43
Anton Gerasimov <dev@zyxmasta.com>
2025-01-26 10:38:35 +0500

    check zfs pool existense helpers added

--- lib.sh
+++ lib.sh
@@ -16,3 +16,25 @@ function ensure {
     echo "Wait ${MESSAGE} ..."
     sleep $WAIT_TIME
 }
+
+# Check if ZFS pool exists
+function is_zfs_pool_exists {
+    local POOL_NAME=$1
+
+    found=`zpool list -H $POOL_NAME 2>/dev/null`
+    if [[ $? -eq 0 ]]; then
+        error_message "${POOL_NAME} found!"
+        exit
+    fi
+}
+
+# Check if ZFS pool does not exists
+function is_zfs_pool_not_exists {
+    local POOL_NAME=$1
+
+    found=`zpool list -H $POOL_NAME 2>/dev/null`
+    if [[ $? -eq 1 ]]; then
+        error_message "${POOL_NAME} not found!"
+        exit
+    fi
+}
